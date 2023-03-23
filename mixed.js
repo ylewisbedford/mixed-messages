40feb6cd4c2c452059dd35f7b684e90789683751
ylewisbedford <yobedford@gmail.com>
2023-03-23 20:19:59 +0000

    defining random word arrays and random index function

---
+++ mixed.js
@@ -0,0 +1,8 @@
+const randWord1 = ['funny', 'silly', 'cute'];
+const randWord2 = ['banana', 'pineapple', 'pear'];
+const randWord3 = ['bum', 'poo', 'wee'] ;
+
+function randIndex() {
+    return Math.floor(Math.random() * 2);
+}
+
