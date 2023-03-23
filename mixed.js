8924b784bbeba7422930f3ed07923d56ada85265
ylewisbedford <yobedford@gmail.com>
2023-03-23 20:41:48 +0000

    adding declarations of random words and making sentence function

--- mixed.js
+++ mixed.js
@@ -4,5 +4,14 @@ const randWord3 = ['bum', 'poo', 'wee'] ;
 
 function randIndex() {
     return Math.floor(Math.random() * 2);
+
+let randomWord1 = randWord1[randIndex()];
+let randomWord2 = randWord2[randIndex()];
+let randomWord3 = randWord3[randIndex()];
+
+function makeSentence() {
+    
+    console.log(`knock knock. Who's there? ${randomWord1}. ${randomWord1} who? ${randomWord1} ${randomWord2} ${randomWord3}`);
 }
 
+makeSentence();
