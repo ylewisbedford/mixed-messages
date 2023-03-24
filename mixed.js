2441d685538698173f91e37580ce5adf7e523aec
ylewisbedford <yobedford@gmail.com>
2023-03-24 17:54:51 +0000

    fix of changes

--- mixed.js
+++ mixed.js
@@ -1,13 +1,14 @@
 const randWord1 = ['funny', 'silly', 'cute'];
 const randWord2 = ['banana', 'pineapple', 'pear'];
-const randWord3 = ['bum', 'poo', 'wee'] ;
+const randWord3 = ['bum', 'fart','poo', 'wee' ] ;
 
 function randIndex() {
     return Math.floor(Math.random() * 2);
-
+}
 let randomWord1 = randWord1[randIndex()];
 let randomWord2 = randWord2[randIndex()];
 let randomWord3 = randWord3[randIndex()];
+
 
 function makeSentence() {
     
